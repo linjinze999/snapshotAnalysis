5440ff94127ce676f63403038247bcba015567f3
jinzelin <jinzelin@tencent.com>
2022-07-25 20:46:29 +0800

    feat: init VuePress

--- src/snapshot/index.ts
+++
@@ -1,6 +0,0 @@
-import * as fs from "fs";
-import V8Snapshot from './V8/V8Snapshot';
-
-const text = fs.readFileSync("xxx").toString();
-const v8 = new V8Snapshot({text});
-console.log(v8.calculateStatistics())
